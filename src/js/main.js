9b7b0d81053d90ea2317f27af7a6efe7112cc88e
石门 <xijieyin@gmail.com>
2023-11-03 21:42:32 +0800

    fix: udpate styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,12 @@
-VANTA.BIRDS({
+VANTA.TOPOLOGY({
   el: "#vanta-element",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 200.0,
-  minWidth: 200.0,
-  scale: 1.0,
-  scaleMobile: 1.0,
-  backgroundColor: 0xffffff,
-  color1: 0x436b91,
-  birdSize: 1.30,
-  separation: 100.00,
-  alignment: 75.00,
-  cohesion: 54.00
-});
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xcaacd,
+  backgroundColor: 0xffffff
+})
